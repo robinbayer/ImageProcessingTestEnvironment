37658b1afef7cb8b84716b898d5ab6310ced3dc9
Robin Bayer <43524021+robinbayer@users.noreply.github.com>
2022-06-26 21:06:09 -0500

    Interim commit

--- Program.cs
+++ Program.cs
@@ -35,25 +35,32 @@ namespace ImageProcessingTestEnvironment
         private async Task Execute(IConfiguration configuration)
         {
 
-            int totalUncompressedBytes = 0;
-            int totalCompressedBytes = 0;
+
+            byte storeByte1 = 0;
+            byte storeByte2 = 0;
+
 
 
             //System.IO.StreamWriter outputFileStreamWriter = new StreamWriter(configuration["AppSettings:OutputFileName"]);
 
-            int rowsPerEncodededBand;
-            int currentRowForEncodedBand;
             int currentArrayIndex;
             string inputFileName;
             byte[] rgbValuesForRow;
             TequaCreek.WGODataModelLibrary.Models.EncodedImage encodedImage;
+            Chilkat.Compression compress = new Chilkat.Compression();
+
+
+            
+
 
             // TEMP CODE
-            inputFileName = "/Users/robinbayer/Documents/junk/radar.tif";
-            rowsPerEncodededBand = 400;
+            inputFileName = "/Users/robinbayer/Documents/junk/example.tif";
 
             // END TEMP CODE
 
+            compress.Algorithm = "deflate";
+            compress.EncodingMode = "base64";
+            compress.DeflateLevel = 9;
 
             encodedImage = new TequaCreek.WGODataModelLibrary.Models.EncodedImage();
 
@@ -65,74 +72,115 @@ namespace ImageProcessingTestEnvironment
             {
 
                 encodedImage.imageHeightPixels = accessor.Height;
-                encodedImage.imageHeightPixels = accessor.Width;
-                encodedImage.pixelRowsPerEncodedBand = rowsPerEncodededBand;
+                encodedImage.imageWidthPixels = accessor.Width;
 
-                currentRowForEncodedBand = 1;
                 for (int y = 0; y < accessor.Height; y++)
                 {
-
                     currentArrayIndex = 0;
-                    currentRowForEncodedBand = 1;
 
                     Span<Rgba32> pixelRow = accessor.GetRowSpan(y);
-                    totalUncompressedBytes += pixelRow.Length * 3;
-                    rgbValuesForRow = new byte[pixelRow.Length * 3 * rowsPerEncodededBand];      // storing RGB byte value for each pixel
+                    rgbValuesForRow = new byte[pixelRow.Length * 3];      // storing RGB byte value for each pixel
 
-                    while (currentRowForEncodedBand <= rowsPerEncodededBand)
+
+                    // TEMP CODE
+                    if (y == 2000)
                     {
+                        storeByte1 = pixelRow[2007].R;
+                        storeByte2 = pixelRow[2331].G;
 
-                        // pixelRow.Length has the same value as accessor.Width,
-                        // but using pixelRow.Length allows the JIT to optimize away bounds checks:
-                        for (int x = 0; x < pixelRow.Length; x++)
-                        {
-                            // Get a reference to the pixel at position x
-                            ref Rgba32 pixel = ref pixelRow[x];
-
-                            rgbValuesForRow[currentArrayIndex++] = pixel.R;
-                            rgbValuesForRow[currentArrayIndex++] = pixel.G;
-                            rgbValuesForRow[currentArrayIndex++] = pixel.B;
-                        }
+                        // TEMP CODE
+                        Console.WriteLine("Stored bytes {0} and {1} for later comparion", storeByte1, storeByte2);
+                        // END TEMP CODE
 
 
-                        y++;
-                        if (y < accessor.Height)
-                        {
-                            pixelRow = accessor.GetRowSpan(y);
-                            totalUncompressedBytes += pixelRow.Length * 3;
-                        }
-                        currentRowForEncodedBand++;
-                    }       // while (currentRowForEncodedBand <= rowsPerEncodededBand)
+                    }
+                    // END TEMP CODE
 
-                    byte[] compressedBase64ByteArray = CompressByteArray(rgbValuesForRow!);
+
+                    // pixelRow.Length has the same value as accessor.Width,
+                    // but using pixelRow.Length allows the JIT to optimize away bounds checks:
+                    for (int x = 0; x < pixelRow.Length; x++)
+                    {
+                        // Get a reference to the pixel at position x
+                        ref Rgba32 pixel = ref pixelRow[x];
+
+                        rgbValuesForRow[currentArrayIndex++] = pixel.R;
+                        rgbValuesForRow[currentArrayIndex++] = pixel.G;
+                        rgbValuesForRow[currentArrayIndex++] = pixel.B;
+                    }
+
+                    /*
+                    byte[] compressedBytes;
+                    compressedBytes = compress.CompressBytes(rgbValuesForRow);
+
+
+                    if (compress.LastMethodSuccess != true)
+                    {
+                        Console.WriteLine(compress.LastErrorText);
+                        return;
+                    }
+                    */
+
+
 
                     //string S = Encoding.UTF8.GetString(B)
 
-                    encodedImage.encodedBands.Add(Convert.ToBase64String(compressedBase64ByteArray, 0,
-                                                  compressedBase64ByteArray.Length, Base64FormattingOptions.None));
+                    encodedImage.encodedBands.Add(compress.CompressBytesENC(rgbValuesForRow));
 
-                    totalCompressedBytes += compressedBase64ByteArray.Length;
 
                 }       // for (int y = 0; y < accessor.Height; y++)
 
             });
 
-            Console.WriteLine("Compressed from {0} bytes to {1} bytes - {2}% compression", totalUncompressedBytes, totalCompressedBytes,
-                              (1 - (float)totalCompressedBytes / (float)totalUncompressedBytes) * 100);
-            Console.ReadKey();
-
-
 
             // TEMP CODE
 
             // serialize JSON directly to a file
+            Console.WriteLine("WRiting output file");
+
             using (StreamWriter file = File.CreateText("/Users/robinbayer/Documents/junk/encodedImage.json"))
             {
                 JsonSerializer serializer = new JsonSerializer();
                 serializer.Serialize(file, encodedImage);
             }
 
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
             // END TEMP CODE
+
+
+            Console.WriteLine("Reversing for comparison");
+
+            TequaCreek.WGODataModelLibrary.Models.EncodedImage encodedImage2;
+
+            // Reverese process to test
+            // deserialize JSON directly from a file
+            using (StreamReader file = File.OpenText("/Users/robinbayer/Documents/junk/encodedImage.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                encodedImage2 = (TequaCreek.WGODataModelLibrary.Models.EncodedImage)serializer.Deserialize(file, typeof(TequaCreek.WGODataModelLibrary.Models.EncodedImage));
+            }
+
+            int counter = 0;
+            foreach (string encodedBand in encodedImage2.encodedBands)
+            {
+
+                byte[] decoded = compress.DecompressBytesENC(encodedBand);
+
+                if (counter == 2000)
+                {
+
+                    Console.WriteLine("Original/New Byte {0}/{1}", storeByte1, decoded[2007 * 3]);
+                    Console.WriteLine("Original/New Byte {0}/{1}", storeByte2, decoded[2331 * 3 + 1]);
+
+                }
+
+                counter++;
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            
 
 
         }       // Execute()
